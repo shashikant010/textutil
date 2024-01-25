bf17a1413a11734287fd614cccac50fa38680576
shashikant010 <shashikantyadav0503@gmail.com>
2024-01-25 17:22:28 +0530

    added to lower case and word count and character count to webpage

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -4,10 +4,20 @@ export default function Textform(prop) {
     const [text,setText]=useState('Enter the text');
     const ontextchange=(event)=>{
         setText(event.target.value)
+
     }
-    const onbtnclick=()=>{
+    const onupbtnclick=()=>{
         let newText=text.toUpperCase();
         setText(newText);
+
+    }
+    const ondownbtnclick=()=>{
+        let newText=text.toLowerCase();
+        setText(newText);
+
+    }
+    const ontextclick=()=>{
+        if(text=="Enter the text") setText("");
     }
   return (
     <div>
@@ -19,12 +29,22 @@ export default function Textform(prop) {
       className="form-control"
       value={text}
         onChange={ontextchange}
+        onClick={ontextclick}
       id="exampleFormControlTextarea1"
       rows={8}
       defaultValue={""}
     />
   </div>
-  <button className="btn btn1 "onClick={onbtnclick}>convert to uppercase</button>
+  <button className="btn btn1 "onClick={onupbtnclick}>convert to uppercase</button>
+  <button className="btn btn2 "onClick={ondownbtnclick}>convert to Lowercase</button>
+
+  <div className="container">
+    <h1>Your text summary</h1>
+    <p>your text contain {text.length} characters and {text.split(" ").length} words</p>
+    <p>It can be read in {text.split(" ").length*0.008} minutes</p>
+    <h2>Preview</h2>
+    <p>{text}</p>
+  </div>
 </>
 
     </div>
