5ddaae3506d24302d0fc588fbd6097f24f855993
shashikant010 <shashikantyadav0503@gmail.com>
2024-01-29 18:16:54 +0530

    added router links routes to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 import React , {useState} from 'react';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 import Alert from './components/Alert';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   const [mode,setmode]=useState('light');
@@ -38,15 +45,24 @@ function App() {
 <Navbar title="textUtils" mode={mode} togglemode={togglemode}/>
 
 <Alert alert={alert}/>
+<Router>
+<Routes>
+          <Route path="/about" element={<About/>}>
+            {/* <About /> */}
+          </Route>
+          <Route path="/" element={<Textform heading="Enter text below to analyze "mode={mode} alert={showalert}/>}>
+          {/* <Textform heading="Enter text below to analyze "mode={mode} alert={showalert}/> */}
+            
+          </Route>
+        </Routes>
+        </Router>
 
 
-<div className="container" >
-<Textform heading="Enter text below to analyze "mode={mode} alert={showalert}/>
-{/* <About/> */}
+
+
 </div>
 
-    </div>
-  );
+  ) 
 }
 
 export default App;
